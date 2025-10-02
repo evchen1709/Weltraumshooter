b07d7dd23f0561f3922e19b105f22f83a2f212f9
evchen1709 <evchen1709@googlemail.com>
2025-10-02 09:35:58 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,33 @@
+// Minimal cache-first service worker for Weltraum Shooter
 const CACHE_NAME = 'weltraum-shooter-v1';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.webmanifest',
-  './icons/icon-192.svg',
-  './icons/icon-256.svg',
-  './icons/icon-384.svg',
-  './icons/icon-512.svg'
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
   );
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    ))
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   const req = event.request;
+  if (req.method !== 'GET' || new URL(req.url).origin !== self.location.origin) {
+    return;
+  }
   event.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).then(resp => {
-      // runtime cache GET requests
-      if (req.method === 'GET' && resp.status === 200) {
-        const clone = resp.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(req, clone));
-      }
-      return resp;
-    }).catch(() => caches.match('./index.html')))
+    caches.match(req).then((cached) => cached || fetch(req))
   );
 });
