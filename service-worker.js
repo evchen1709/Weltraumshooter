7ca3beb64dc80958468f28bfa722a2eff2e9946f
evchen1709 <evchen1709@googlemail.com>
2025-10-03 17:40:26 +0000

    Service Worker: Cache-Busting aktiviert, immer neueste Version laden

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,27 @@
-const CACHE='ws-noicons-v5';
-const ASSETS=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{const r=e.request; if(r.method!=='GET'||new URL(r.url).origin!==self.location.origin) return;
-  e.respondWith(caches.match(r).then(res=>res||fetch(r)));
+const CACHE = 'ws-noicons-v6';
+const ASSETS = ['./', './index.html', './manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  const r = e.request;
+  if (r.method !== 'GET' || new URL(r.url).origin !== self.location.origin) return;
+  e.respondWith(
+    fetch(r)
+      .then(res => {
+        // Update Cache
+        const resClone = res.clone();
+        caches.open(CACHE).then(c => c.put(r, resClone));
+        return res;
+      })
+      .catch(() => caches.match(r))
+  );
 });
